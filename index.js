d95f90256ded5d1d84c9cd0583a6951b71b11b73
tee9900 <asakpatega@gmail.com>
2022-10-01 08:45:58 +0100

    Commit file

---
+++ index.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    var url = 'http://hn.algolia.com/api/v1/search_by_date?query=javascript'
+    $.getJSON(url, function(data){
+
+        var currentQuote = ''
+        var quotes = data.hits
+
+        $('.read').on('click', function(){
+            currentQuote = quotes[Math.floor(Math.random() * quotes.length)]
+            console.log(currentQuote)
+            $(".quoteBody").hide()
+            $('.quote').html(currentQuote.title).attr('href', currentQuote.url).attr('target', '_blank')
+            $('.quoteAuthor').html(currentQuote.author)
+
+            $('.tweetQuote').attr('href','https://www.google.com/search?client=firefox-b-d&q= '+ currentQuote.title)
+            .attr('target', '_blank')
+            .attr('disabled', false)
+
+            $('.read').html('next news')
+        })
+
+        // console.log(data)
+        // console.log(quotes[0])
+    })
+})
